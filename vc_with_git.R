749eaa09a042e65a8f1fb21d787cf3411319ac7c
virtualarchitectures <virtualarchitectures@gmail.com>
2021-10-25 20:04:11 +0100

    Added function for initialize and push to remote

--- vc_with_git.R
+++ vc_with_git.R
@@ -1,5 +1,6 @@
-# load library for working with Git
+# load libraries for working with Git
 library(usethis)
+library(gitcreds)
 
 # edit your gitconfig file from R e.g. to add name and email credentials
 # edit_git_config()
@@ -13,4 +14,13 @@ getwd()
 # initialize a new local git repository from within R
 # use_git()
 
-# stage and commit the changes in the Git pane
+# stage and commit the changes to the local repository using the Git pane
+
+# setup a Personal Access Token (PAT) on GITHub
+# create_github_token()
+
+# save PAT using gitcreds package
+# gitcreds_set()
+
+# create a remote github repository for the current project
+use_github()

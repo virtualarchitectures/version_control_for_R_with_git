4318cb38668ae99a109115fdcceb9bb4183a59ea
virtualarchitectures <virtualarchitectures@gmail.com>
2021-10-25 19:38:13 +0100

    Initial commit

---
+++ vc_with_git.R
@@ -0,0 +1,16 @@
+# load library for working with Git
+library(usethis)
+
+# edit your gitconfig file from R e.g. to add name and email credentials
+# edit_git_config()
+
+# check working directory is correct
+getwd()
+
+# set working directory if required
+# setwd("<path here>")
+
+# initialize a new git repository from within R
+# use_git()
+
+use_git()
